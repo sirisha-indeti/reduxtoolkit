3af21a4d592713b7854cee67eee5f654ee205e3e
sirisha-indeti <indetisirisha123@gmail.com>
2026-04-27 15:29:07 +0530

    'cart'

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import React, { use } from 'react'
+import { removecart } from '../features/cart/cartSlice'
+import { useSelector } from 'react-redux'
+import { useDispatch } from 'react-redux'
 
 function Cart() {
+    const dispatch=useDispatch()
+    const {items,total}=useSelector((state)=>state.cart)
   return (
     <div>
-      cart
+      {items.length==0?(<div>no products are added</div>):
+  
+      
+      (<div>{items.map((i)=>(
+        <div key={i.id}>
+            <div>{i.name}-{i.price} * {i.quantity}</div>
+            <button onClick={()=>dispatch(removecart(i.id))}>remove</button>
+        </div>
+      ))}</div>)
+      }
+
+      <h1>{total}</h1>
     </div>
   )
 }
--- src/features/cart/cartSlice.js
+++ src/features/cart/cartSlice.js
@@ -10,9 +10,29 @@ const cartSlice=createSlice({
     initialState,
     reducers:{
         addtocart:(state,action)=>{
+
+            const existingitem=state.items.find((i)=>i.id==action.payload.id)
+
+            if(existingitem){
+                existingitem.quantity+=1
+            }else{
+
+                state.items.push({...action.payload,quantity:1})
+            }
             console.log('add to cart')
+
+            state.total+=action.payload.price
         },
         removecart:(state,action)=>{
+const item=state.items.find((i)=>i.id==action.payload)
+
+if (item){
+    state.total-=item.price*item.quantity
+}
+            console.log(action.payload)
+            state.items=state.items.filter((i)=>i.id!=action.payload)
+
+            
             console.log('removed')
         }
     }
